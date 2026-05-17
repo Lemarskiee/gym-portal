baa053ff482a9d76ec0a854c7f3159f2a3bacb11
Lemar <pradillalemar2@gmail.com>
2026-05-18 05:58:23 +0800

    membership and admin

--- database/migrations/2026_05_12_044717_create_members_table.php
+++ database/migrations/2026_05_12_044717_create_members_table.php
@@ -10,23 +10,23 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('members', function (Blueprint $table) {
-            $table->id();
+{
+    Schema::create('members', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete(); // add this
+        $table->foreignId('membership_plan_id')
+            ->constrained()
+            ->cascadeOnDelete();
 
-            $table->foreignId('membership_plan_id')
-                ->constrained()
-                ->cascadeOnDelete();
+        $table->string('first_name');
+        $table->string('last_name');
+        $table->string('email')->unique();
+        $table->string('phone')->nullable();
+        $table->date('start_date');
 
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('phone')->nullable();
-            $table->date('start_date');
-
-            $table->timestamps();
-        });
-    }
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
